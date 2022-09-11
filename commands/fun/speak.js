f1f3e3ced947fafadbae515b56a45d68a9d46530
minhkhoa333 <mklift333@gmail.com>
2022-09-11 18:43:28 +0700

    Kho Bot

---
+++ commands/fun/speak.js
@@ -0,0 +1,53 @@
+const { EmbedBuilder } = require('discord.js')
+const { getAudioUrl } = require('google-tts-api')
+const { createAudioPlayer, createAudioResource, joinVoiceChannel, getVoiceConnection, StreamType } = require('@discordjs/voice')
+
+module.exports = {
+    name: 'speak',
+    aliases: ['suc', 'talk'],
+    category: 'fun',
+    run: async (client, message, args) => {
+        if (!args[0]) return message.channel.send('Cái lồn má đéo nhập gì sao nói')
+        const string = args.join(' ')
+        if (string.length > 200) return message.channel.send('Đụ đĩ mẹ nhập ít thôi')
+        const voiceChannel = message.member.voice.channel
+        if (!voiceChannel) return message.reply('Dô room đi thằng lồn')
+        try {
+            // const exampleEmbed = new EmbedBuilder()
+            // .setColor(0x0099FF)
+            // .setTitle('Some title')
+            // .setURL('https://discord.js.org/')
+            // .setAuthor({ name: 'Some name', url: 'https://discord.js.org' })
+            // .setDescription('Some description here')
+            // .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+            // .addFields(
+            //     { name: 'Regular field title', value: 'Some value here' },
+            //     { name: '\u200B', value: '\u200B' },
+            //     { name: 'Inline field title', value: 'Some value here', inline: true },
+            //     { name: 'Inline field title', value: 'Some value here', inline: true },
+            // )
+            // .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+            // .setImage('https://i.imgur.com/AfFp7pu.png')
+            // .setTimestamp()
+            // .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+            joinVoiceChannel({
+                channelId: voiceChannel.id,
+                guildId: message.guild.id,
+                adapterCreator: message.guild.voiceAdapterCreator,
+            })
+            const stream = getAudioUrl(args.join(' '), {
+                lang: 'vi',
+            });
+            const player = createAudioPlayer();
+            const resource = createAudioResource(stream, {inlineVolume: true});
+            player.play(resource);
+
+            const connection = getVoiceConnection(message.guild.id);
+            connection.subscribe(player);
+            setTimeout(() => connection.destroy(), 30000)
+        } catch(e) {
+            message.channel.send('Kêu nữa tao dả dô cái mỏ lồn nha')
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,40 @@
+const { Client, Collection, IntentsBitField, Partials } = require('discord.js')
+const myIntents = new IntentsBitField();
+myIntents.add(IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.DirectMessages, IntentsBitField.Flags.DirectMessageReactions, IntentsBitField.Flags.GuildMessageReactions, IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.GuildVoiceStates);
+const client = new Client({intents: myIntents, partials: [Partials.Message, Partials.Channel, Partials.Reaction]})
+const { token } = require('./config.json')
+
+client.on('ready', () => {
+    console.log(`${client.user.username} nè`)
+
+    client.user.setPresence({
+        activities: [{
+            name: 'Đang sục',
+            type: 'Playing',
+        }],
+        status: 'online'
+    })
+})
+
+client.commands = new Collection()
+client.aliases = new Collection()
+
+const commandAlias = ['command']
+
+commandAlias.forEach(handler => {
+    require(`./handlers/${handler}`)(client)
+})
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return
+    const prefix = '/'
+    if (!message.content.startsWith(prefix)) return
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const cmd = args.shift().toLowerCase()
+    if (cmd.length === 0) return
+    let command = client.commands.get(cmd)
+    if (!command) command = client.commands.get(client.aliases.get(cmd))
+    if (command) command.run(client, message, args)
+})
+
+client.login(token)
